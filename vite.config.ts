81c4d58dc79a4660493aa3dcecad3200956f003a
Naboraj Sarkar <nishant.ns.business@gmail.com>
2026-08-18 15:30:26 +0530

    chore(vite): clean unused config arguments in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig, ConfigEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }: ConfigEnv) => ({
+export default defineConfig(() => ({
   server: {
     host: "0.0.0.0",
     port: 5000,
